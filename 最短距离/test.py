d7a0e4af3c45611caa54ba814318fd66e17930a1
tlt18 <tlt18@mails.tsinghua.edu.cn>
2020-12-11 18:04:48 +0800

    base

--- 最短距离/test.py
+++ 最短距离/test.py
@@ -1,47 +1,83 @@
 import matplotlib.pyplot as plt
 import numpy as np
-# x=np.linspace(-1,1,50)
+
 DIST_MAX=10
-# len(station)=8
-# x=[-12.856353,-5.68875,27.818487,-19.774317,18.884097,-23.383704,24.125517,-11.09844]
-# y=[1.524696,-2.168163,-12.002319,20.724144,-16.146504,6.219774,3.064488,7.70784]
-station=[[-12.856353,1.524696],[-5.68875,-2.168163],[27.818487,-12.002319],[-19.774317,20.724144],[18.884097,-16.146504],[-23.383704,6.219774],[24.125517,3.064488],[-11.09844,7.70784]]
+station=np.array([[-12.856353,1.524696],[-5.68875,-2.168163],[27.818487,-12.002319],[-19.774317,20.724144],[18.884097,-16.146504],[-23.383704,6.219774],[24.125517,3.064488],[-11.09844,7.70784]])
+solve_list=[]
+start=np.array([0,-10])
+end=np.array([-21,20])
 
-start=[0,-10]
-end=[-21,20]
-
+# 画出连接ij两个站点的线段
 def draw_line(i,j,c=0):
     color=['r','b']
     plt.plot([station[i][0],station[j][0]],[station[i][1],station[j][1]],Color=color[c],Linewidth=2)
-    # print(station[i])
 
+# 画出散点
 def draw_point():
     plt.grid()
     for point in station:
-        plt.scatter(point[0],point[1],color=[1,0.9,0],label="Transfer Station")
-        plt.text(point[0]+1,point[1]+1,station.index(point))
-
-
+        plt.scatter(point[0],point[1],color=[1,0.9,0])
+        plt.text(point[0]+1,point[1]+1,(np.argwhere(station==point)[0][0]))
     plt.scatter(0,0,s=200,marker='*',color='r',label="Centre")
 
+# 画出端点和点i的连线
+def draw_link(endpoint,i,c=0):
+    color=['r','b']
+    plt.plot([station[i][0],endpoint[0]],[station[i][1],endpoint[1]],Color=color[c],Linewidth=2)
+
+# 对于一个端点返回最近的点的索引
+def find_closed(endpoint):
+    dist=[]
+    for point in station:
+        dist.append(np.linalg.norm(endpoint-point)) 
+    return dist.index(min(dist))
+
+def solve(now,close_end):
+    next = find_next(now,close_end)
+    if(next==close_end):
+        return
+    solve_list.append(next)
+    solve(next,close_end)
+
+def find_next(now,close_end):
+    if(np.linalg.norm( station[now]-station[close_end] ) < DIST_MAX):
+        return close_end
+    dist=[]
+    for point in station:
+        np.dot(station[close_end]-station[now],point-station[now])
+    # 对内积序列进行小到大排序，argsort返回索引
+    close_index_list=np.argsort(dist)
+    for index in close_index_list[::-1]:
+        if( np.linalg.norm(station[now]-station [index] ) < DIST_MAX ):
+            return index
+    # 所有投影都不满足，直接走向最终点
+    return close_end
 
 if __name__ == '__main__':
+    # 画散点
     draw_point()
     # 画端点
     plt.scatter([start[0],end[0]],[start[1],end[1]])
-    # # 画路径
-    # plt.plot([end[0],x[3]],[end[1],y[3]],color='r',Linewidth=2)
-    draw_line(3, 5)
-    # draw_line(5,0)
-    # draw_line(1,0)
-    # plt.plot([start[0],x[1]],[start[1],y[1]],color='r',Linewidth=2)
-    # plt.text(start[0]+1,start[1]+1,'start')
-    # plt.text(end[0]-2.,end[1]-2,'end')
-    # plt.legend(loc='best')
+    plt.text(start[0]+1,start[1]+1,'start')
+    plt.text(end[0]-1,end[1]-1,'end')
+    # 画路径
+    # 找最近点
+    close_end=find_closed(end)
+    close_start=find_closed(start)
+    draw_link(end,close_end)
+    draw_link(start,close_start)
+    # 寻点存在solve_list中
+    solve_list.append(close_start).append(close_start)
+    solve(close_start,close_end)
+    solve_list.append(close_end)
+    print(solve_list)
+    for i in range(len(solve_list)):
+        draw_line(solve_list[i-1],solve_list[i])
+
+    # 画图
+    plt.legend(loc='best')
     plt.show()
-    # dist=[]
-    # for i in range(len(x)):
-    #     dist.append(np.linalg.norm(start,[x[i-1],y[i-1]]))
+
 
 
 
